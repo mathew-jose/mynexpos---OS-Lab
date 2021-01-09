03352564bc0d0815402318353c07fbcc7ce37695
mathew-jose <mathewjose2409@gmail.com>
2021-01-09 17:30:10 +0530

    First Commit

---
+++ myexpos/xfs-interface/batch.sh
@@ -0,0 +1,49 @@
+load --int=timer $HOME/myexpos/spl/spl_progs/timer_stage27.xsm
+load --exec $HOME/myexpos/expl/samples/a1_27.xsm
+load --exec $HOME/myexpos/expl/samples/a2_27.xsm
+load --exec $HOME/myexpos/expl/samples/a3_27.xsm
+load --exec $HOME/myexpos/expl/samples/a4_27.xsm
+load --exec $HOME/myexpos/expl/samples/a5_27.xsm
+load --exec $HOME/myexpos/expl/samples/m_store.xsm
+load --exec $HOME/myexpos/expl/samples/m_sort.xsm
+load --exec $HOME/myexpos/expl/samples/m_merge.xsm
+load --exec $HOME/myexpos/expl/samples/pgm1.xsm
+load --exec $HOME/myexpos/expl/samples/pgm2.xsm
+load --exec $HOME/myexpos/expl/samples/gpid_21.xsm
+load --data $HOME/myexpos/sample.dat
+load --exec $HOME/myexpos/expl/samples/a1_24.xsm
+load --exec $HOME/myexpos/expl/samples/a4_25.xsm
+load --exec $HOME/myexpos/expl/samples/lu_26.xsm
+load --exec $HOME/myexpos/expl/samples/ru_26.xsm
+load --exec $HOME/myexpos/expl/samples/create.xsm
+load --int=console $HOME/myexpos/spl/spl_progs/console_17.xsm
+load --int=disk $HOME/myexpos/spl/spl_progs/disk_interrupt_stage19.xsm
+load --library ../expl/library.lib
+load --exhandler $HOME/myexpos/spl/spl_progs/exception_stage19.xsm
+load --int=4 $HOME/myexpos/spl/spl_progs/int4_stage23.xsm
+load --int=5 $HOME/myexpos/spl/spl_progs/int5_stage25.xsm
+load --int=7 $HOME/myexpos/spl/spl_progs/int7_stage25.xsm
+load --int=6 $HOME/myexpos/spl/spl_progs/int6_stage24.xsm
+load --int=8 $HOME/myexpos/spl/spl_progs/int8_stage24.xsm
+load --int=10 $HOME/myexpos/spl/spl_progs/int10_stage20.xsm
+load --int=12 $HOME/myexpos/spl/spl_progs/int12_stage26.xsm
+load --int=11 $HOME/myexpos/spl/spl_progs/int11_stage21.xsm
+load --int=13 $HOME/myexpos/spl/spl_progs/int13_stage22.xsm
+load --int=14 $HOME/myexpos/spl/spl_progs/int14_stage22.xsm
+load --int=15 $HOME/myexpos/spl/spl_progs/int15_stage26.xsm
+load --int=16 $HOME/myexpos/spl/spl_progs/int16_stage26.xsm
+load --int=17 $HOME/myexpos/spl/spl_progs/int17_stage26.xsm
+load --int=5 $HOME/myexpos/spl/spl_progs/int5_stage25.xsm
+load --init $HOME/myexpos/expl/samples/init_26.xsm
+load --idle $HOME/myexpos/expl/samples/idle_stage12.xsm
+load --shell $HOME/myexpos/expl/samples/shell_26.xsm
+load --module 0 $HOME/myexpos/spl/spl_progs/module0_stage24.xsm
+load --module 4 $HOME/myexpos/spl/spl_progs/module4_stage23.xsm
+load --module 7 $HOME/myexpos/spl/spl_progs/boot_module_stage27.xsm
+load --module 5 $HOME/myexpos/spl/spl_progs/module5_stage27.xsm
+load --module 2 $HOME/myexpos/spl/spl_progs/module2_stage27.xsm
+load --module 6 $HOME/myexpos/spl/spl_progs/module6_stage27.xsm
+load --module 1 $HOME/myexpos/spl/spl_progs/module1_stage25.xsm
+load --module 3 $HOME/myexpos/spl/spl_progs/module3_stage25.xsm
+load --int=9 $HOME/myexpos/spl/spl_progs/int9_stage19.xsm
+load --os $HOME/myexpos/spl/spl_progs/os_startup_stage27.xsm
